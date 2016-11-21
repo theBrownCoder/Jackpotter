11bd959080cf1411ee3292ec3b1288bf7240a63f
theBrownCoder <thebrowncoder@gmail.com>
2016-11-21 14:59:27 -0500

    * .gitignore: Just added testing for buttons pertaining to each of   the 6 games to be included...will edit the activity switching   next...
    * Jackpotter.sln:
    * MainActivity.cs:
    * Jackpotter.csproj:
    * AboutAssets.txt:
    * Main.axml:
    * AssemblyInfo.cs:
    * Strings.xml:
    * AboutResources.txt:
    * AndroidManifest.xml:
    * Resource.designer.cs:
    * Icon.png:
    * Icon.png:
    * Icon.png:
    * Icon.png:
    * Icon.png:

---
+++ Jackpotter/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Jackpotter
+{
+	[Activity(Label = "Jackpotter", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button>(Resource.Id.myButton);
+			Button button1 = FindViewById<Button>(Resource.Id.pick3Button);
+			Button button2 = FindViewById<Button>(Resource.Id.pick4Button);
+			Button button3 = FindViewById<Button>(Resource.Id.cash5Button);
+			Button button4 = FindViewById<Button>(Resource.Id.Luck4LifeButton);
+			Button button5 = FindViewById<Button>(Resource.Id.megamillsButton);
+			Button button6 = FindViewById<Button>(Resource.Id.powerballButton);
+
+
+			button.Click += delegate { button.Text = $"{count++} clicks!"; };
+			button1.Click += delegate { button1.Text = $"{count++} clicks!"; };
+			button2.Click += delegate { button2.Text = $"{count++} clicks!"; };
+			button3.Click += delegate { button3.Text = $"{count++} clicks!"; };
+			button4.Click += delegate { button4.Text = $"{count++} clicks!"; };
+			button5.Click += delegate { button5.Text = $"{count++} clicks!"; };
+			button6.Click += delegate { button6.Text = $"{count++} clicks!"; };
+		}
+	}
+}
+
